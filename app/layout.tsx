b0b415da3940676a6139a75e8d742029ef8b9956
unknown <it.haksinterlance@gmail.com>
2024-05-11 01:39:29 -0700

    commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header/header'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next js 14 Ecommerce',
+  description: 'Modern Ecommerce Website',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>
+              Copyright &copy; 2024 - All rights reserved by Next js 14
+              Ecommerce
+            </p>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
