0b608e398b12c6f8abc3bb3eacf01b8e6c95cf40
unknown <jsafroze@gmail.com>
2024-05-24 04:13:02 -0700

    commit

---
+++ lib/hooks/useCartStore.tsx
@@ -0,0 +1,61 @@
+import { create } from 'zustand'
+import { round2 } from '../utils'
+import { OrderItem } from '../models/OrderModel'
+
+type Cart = {
+  items: OrderItem[]
+  itemsPrice: number
+  taxPrice: number
+  shippingPrice: number
+  totalPrice: number
+}
+
+const initialState: Cart = {
+  items: [],
+  itemsPrice: 0,
+  taxPrice: 0,
+  shippingPrice: 0,
+  totalPrice: 0,
+}
+export const cartStore = create<Cart>(() => initialState)
+
+export default function useCartService() {
+  const { items, itemsPrice, taxPrice, shippingPrice, totalPrice } = cartStore()
+  return {
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    increase: (item: OrderItem) => {
+      //here we are also returning a function called increase. We use this item to add or increase an item in the cart. it accepts "item" of type OrderItem
+      //here we are going to update the item in the cart and prices.
+      const exist = items.find((x) => x.slug === item.slug)
+      const updatedCartItems = exist
+        ? items.map((x) =>
+            x.slug === item.slug ? { ...exist, qty: exist.qty + 1 } : x
+          )
+        : [...items, { ...item, qty: 1 }]
+      //if the item already exist in the cart (i.e) exist variable returns null after finding, then update the cart to add qty
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems)
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      })
+    },
+  }
+}
+
+const calcPrice = (items: OrderItem[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + item.price * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 100),
+    taxPrice = round2(Number(0.15 * itemsPrice)),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice)
+  return { itemsPrice, shippingPrice, taxPrice, totalPrice }
+}
